a4db29a284d9bb9338ba07091cbd3fc5efdd478b
Eduar <donsecun@gmail.com>
2023-01-28 12:35:42 +0100

    Created the grid by using js, I used: setPropertty with --grid-rows and --grid-cols, after that a for with  createElement, classList.add, textContentand appendChild; and can change the color from white to blue, creating a htmlcolcollection with getElementByClass, inside a for used addEventListener and target.style.backgroundColor, both for inside a function and dont have problem with the run of js; link inside html file goes in the end of the body

--- main.js
+++ main.js
@@ -4,11 +4,35 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
-    const cell = document.createElement('div');
+    var cell = document.createElement('div');
     cell.classList.add('grid-item');
-    //cell.textContent = (c+1);
+    cell.textContent = (c+1);
     container.appendChild(cell);
+    //console.log('create cell ' +(cell.classList));
   };
+  
+  var change = document.getElementsByClassName('grid-item');
+  //console.log('this '+change+' '+(change.length));
+
+  for (var i = 0 ; i < change.length; i++) {
+    //console.log('inside for');
+    change[i].addEventListener('click', function (e) {
+    //console.log('inside click');
+    e.target.style.backgroundColor = 'blue';
+    });
+ }
+
 };
+  
+//const change1 = document.getElementsByClassName('.grid-item');
+//change1.style.background = 'black';
+/*function toggleModal(){
+  div.classList.toggle('show');  
+}
+//change1.addEventListener('click', toggleModal);*/
+
+//change1.addEventListener('click', function (e) {
+//  e.target.style.background = 'blue';
+//});
 
 makeRows(16, 16);
