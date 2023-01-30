26f137eccda74773f907adc60ec44d308af389b0
Eduar <donsecun@gmail.com>
2023-01-30 10:47:01 +0100

    Now every box toggle color between white and blue

--- main.js
+++ main.js
@@ -8,31 +8,14 @@ function makeRows(rows, cols) {
     cell.classList.add('grid-item');
     cell.textContent = (c+1);
     container.appendChild(cell);
-    //console.log('create cell ' +(cell.classList));
   };
-  
-  var change = document.getElementsByClassName('grid-item');
-  //console.log('this '+change+' '+(change.length));
 
+  var change = document.querySelectorAll('.grid-item');
   for (var i = 0 ; i < change.length; i++) {
-    //console.log('inside for');
     change[i].addEventListener('click', function (e) {
-    //console.log('inside click');
-    e.target.style.backgroundColor = 'blue';
+    e.target.classList.toggle('show');
     });
  }
-
+ 
 };
-  
-//const change1 = document.getElementsByClassName('.grid-item');
-//change1.style.background = 'black';
-/*function toggleModal(){
-  div.classList.toggle('show');  
-}
-//change1.addEventListener('click', toggleModal);*/
-
-//change1.addEventListener('click', function (e) {
-//  e.target.style.background = 'blue';
-//});
-
 makeRows(16, 16);
